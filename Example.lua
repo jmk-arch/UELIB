14cdccb8ed3ecac525ab030d38a8eff465a05aac
west <joinewith@gmail.com>
2026-06-01 20:31:23 +0900

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -3,7 +3,7 @@
 
 local repo = 'https://raw.githubusercontent.com/jmk-arch/UELIB/main/'
 
-local Library = loadstring(game:HttpGet(repo .. 'Library.lua'))()
+local Library = loadstring(game:HttpGet(repo .. 'test-branch.lua'))()
 local ThemeManager = loadstring(game:HttpGet(repo .. 'addons/ThemeManager.lua'))()
 local SaveManager = loadstring(game:HttpGet(repo .. 'addons/SaveManager.lua'))()
 
